c7a2517935260a627fc42d2d109e734b572987e4
Angular CLI <angular-cli@angular.io>
2017-06-20 14:00:05 -0500

    chore: initial commit from @angular/cli

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { GithubAppPage } from './app.po';
+
+describe('github-app App', () => {
+  let page: GithubAppPage;
+
+  beforeEach(() => {
+    page = new GithubAppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!!');
+  });
+});
